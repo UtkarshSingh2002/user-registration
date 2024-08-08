b1b7cff4dd8f4c3356afac5a4ec3994535c9e445
UtkarshSingh <133907862+UtkarshSingh2002@users.noreply.github.com>
2024-08-08 14:10:31 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.log("Database connection failed", err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
